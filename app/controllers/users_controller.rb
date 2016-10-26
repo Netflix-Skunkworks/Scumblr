81d3b1f69c7e885feab437fa11cbeeec2c341d8b
Andrew Hoernecke <ahoernecke@netflix.com>
2016-10-26 11:18:20 -0700

    Update to user parameters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -103,6 +103,6 @@ class UsersController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :admin, :disabled)
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
