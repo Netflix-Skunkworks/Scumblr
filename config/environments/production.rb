eeac926333f6e349ae38fb90a1b45aabc172177d
Andrew Hoernecke <ahoernecke@netflix.com>
2017-04-11 15:29:03 -0700

    Allow serving static assets. Enable caching

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,9 @@ Scumblr::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
+  config.static_cache_control = 'public, max-age=604700'
+
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
