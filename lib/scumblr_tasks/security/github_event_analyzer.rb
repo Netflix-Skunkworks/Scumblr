c3984a399787f01eb89ddd8a2e177259efdae06c
Scott Behrens <sbehrens@netflix.com>
2017-06-21 08:42:10 -0700

    fixes a copy/paste error

--- lib/scumblr_tasks/security/github_event_analyzer.rb
+++ lib/scumblr_tasks/security/github_event_analyzer.rb
@@ -1,4 +1,4 @@
-test/models/events_test.rb#     Copyright 2014 Netflix, Inc.
+#     Copyright 2014 Netflix, Inc.
 #
 #     Licensed under the Apache License, Version 2.0 (the "License");
 #     you may not use this file except in compliance with the License.
